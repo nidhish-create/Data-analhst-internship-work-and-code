d8ffd021ec84ec0d2d832b0ea451f7b1bf165ea2
Nidhish Malviya <72844520+nidhish-create@users.noreply.github.com>
2023-06-06 22:26:28 +0530

    Update Patiend_surgery_details-details about how many surgeries of patients are completed over past 2 years.py

--- Patiend_surgery_details-details about how many surgeries of patients are completed over past 2 years.py
+++ Patiend_surgery_details-details about how many surgeries of patients are completed over past 2 years.py
@@ -8,7 +8,7 @@ import logging
 from service.base_functions import msg_to 
 from datetime import datetime , timedelta
 from service.send_mail_client import send_email
-# from s3_utils.utils import upload_to_s3
+# from s3_utils.utils import upload_to_s3                
 import numpy as np 
 from google.cloud import bigquery           
 import numpy as np 
